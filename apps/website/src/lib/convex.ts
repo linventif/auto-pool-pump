8ba6d85bb94b815dc4a7e3cbda535465f86cd6e5
linventif <gregoire.becue@proton.me>
2025-07-24 01:03:27 +0200

    reset

--- apps/website/src/lib/convex.ts
+++
@@ -1,5 +0,0 @@
-import { ConvexReactClient } from 'convex/react';
-
-const convexUrl = import.meta.env.VITE_CONVEX_URL || 'http://localhost:3210';
-
-export const convex = new ConvexReactClient(convexUrl);
